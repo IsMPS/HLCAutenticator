ff148bd8b71e8b9f42ecc77865ff8e431744c2a0
Isabel Maye <is.ma.pi.rrcc@gmail.com>
2023-01-25 13:36:54 +0100

    Base del proyecto

---
+++ src/hlc/ud04/appsec/interfaz/Interfaz.java
@@ -0,0 +1,18 @@
+package hlc.ud04.appsec.interfaz;
+
+import hlc.ud04.appsec.seguridad.autenticacion.InterfazAutenticacion;
+import hlc.ud04.appsec.seguridad.controlacceso.ControlAcceso;
+
+/**
+ * Interfaz de usuario de la aplicación
+ * @author mmontoro
+ *
+ */
+public interface Interfaz {
+
+  /**
+   * Inicia la aplicación. Aqui hay que poner el código que forma la aplicación
+   */
+  void run();
+
+}
